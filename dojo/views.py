e8a1ae791f442ed3d836251d9225b10a7d616491
lsjhome <lsjhome007@gmail.com>
2018-02-08 23:59:11 +0900

    dojo.views.hello : 한글 문자열 매칭

--- dojo/views.py
+++ dojo/views.py
@@ -8,3 +8,6 @@ def mysum(request, numbers):
     # numbers = "1/2/12/123/12312/312/123123"
     result = sum(list(map(lambda s: int(s or 0), numbers.split("/"))))
     return HttpResponse(result)
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
