0aeebc4502603a50472fb1fc2e3dc6d94f9a2903
lsjhome <lsjhome007@gmail.com>
2018-02-08 23:38:02 +0900

    dojo.views.mysum에서 x/y/z 인자 3개를 처리

--- dojo/views.py
+++ dojo/views.py
@@ -1,3 +1,9 @@
+# dojo/views.py
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+
+def mysum(request, x, y=0, z=0):
+    # request: HttpRequest
+    return HttpResponse(int(x)+int(y)+int(z))
